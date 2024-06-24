e8dbb7cd60b5dd3f808060406ae005c3300f7706
AishwaryaRamkrish <reachaishoo@gmail.com>
2024-06-24 20:45:34 +0000

    Learning about conditional if

---
+++ loops.py
@@ -0,0 +1,13 @@
+
+
+def main():
+
+    fruits = {"apple": 2, "orange": 3, "plum": 5, "grapes": 8}
+    for x,y in fruits.items():
+        print(x,y)
+        print(fruits.items())
+
+
+
+if __name__ == "__main__":
+    main()
